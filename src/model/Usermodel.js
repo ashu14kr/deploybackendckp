f88048de76ae586a3acacf98bac76cb7cc491719
Ashutosh kumar <ashuh14th@gmail.com>
2022-06-17 14:37:25 +0530

    love

--- src/model/Usermodel.js
+++ src/model/Usermodel.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
 
+    _id:{},
     username:{},
     mobileno:{},
-    emailid:{},
+    emailid:{
+        unique: true
+    },
     password:{},
 
 })
