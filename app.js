6085cb0ed4c615976cdb0b7752c627c5c882caa5
Ashutosh kumar <ashuh14th@gmail.com>
2022-06-23 22:23:41 +0530

    update

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Methods",'GET','PUT','POST','DELETE');
+  res.header("Access-Control-Allow-Headers",'Content-Type');
+  next();
+
+})
+
 // users APIS
 
 app.post("/addverifiedUsers", (req, res) => {
