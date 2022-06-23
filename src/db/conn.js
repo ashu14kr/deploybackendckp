cfecd9897a74ff5fe8d3f33112721ea46a42fc54
Ashutosh kumar <ashuh14th@gmail.com>
2022-06-23 08:04:57 +0530

    done

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://3.110.204.227/cricketapp").then(() => {
+mongoose.connect("mongodb://localhost/cricketapp").then(() => {
     console.log("connection successful");
 }).catch((e) => {
     console.log("connection failed");
